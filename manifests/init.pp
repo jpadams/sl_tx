4057173772e89234112f45e4afe012cb43a0f2f5
Jeremy Adams <jeremy@puppetlabs.com>
2015-02-08 15:27:01 -0800

    initial commit

---
+++ manifests/init.pp
@@ -0,0 +1,24 @@
+# This class deploys an installer from the master,
+# installs it, and removes the installer to clean up.
+class sl_tx {
+  # temporarily stage the sl rpm so we can install the software.
+  transition { 'stage sl rpm':
+    resource   => File['/tmp/sl-5.02-1.el6.x86_64.rpm'],
+    attributes => { ensure => present, source => 'puppet:///modules/sl_tx/sl-5.02-1.el6.x86_64.rpm' },
+    prior_to   => Package['sl'],
+  }
+
+  package { 'sl':
+    ensure   => installed,
+    provider => rpm,
+    source   => '/tmp/sl-5.02-1.el6.x86_64.rpm',
+    # this before (or a require in file below) is required
+    # for desired results. 
+    before   => File['/tmp/sl-5.02-1.el6.x86_64.rpm'],
+  }
+
+  # clean up installer file
+  file { '/tmp/sl-5.02-1.el6.x86_64.rpm':
+    ensure => absent,
+  }
+}
